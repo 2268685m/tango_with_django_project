b8e190b169afc61305a5fae1c958caea8b0d4225
2268685m <2268685m@student.gla.ac.uk>
2018-02-02 20:18:44 +0000

    Towards end of chapoter 6, finished Demo.

--- rango/urls.py
+++ rango/urls.py
@@ -5,4 +5,11 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
     
+    # \w denotes a sequence of alphanumeric characters
+    # \- denotes any hyphens
+    # [\w\-]+ denotes as many of these combined as we like
+    # The sequence will be stored in the parameter <category_name_slug>
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
+        views.show_category, name='show_category'),
+    
 ]
