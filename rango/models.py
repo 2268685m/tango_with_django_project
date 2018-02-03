f4ad800bd497048f7e60b4f75ac113bc68839b80
2268685m <2268685m@student.gla.ac.uk>
2018-02-03 12:23:49 +0000

    End of chapter 7.

--- rango/models.py
+++ rango/models.py
@@ -4,7 +4,8 @@ from django.template.defaultfilters import slugify
 # Create your models here.
 
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
+    max_length = 128
+    name = models.CharField(max_length=max_length, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
--- rango/urls.py
+++ rango/urls.py
@@ -4,12 +4,13 @@ from rango import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
+    url(r'^add_category/$', views.add_category, name='add_category'),
     
     # \w denotes a sequence of alphanumeric characters
     # \- denotes any hyphens
     # [\w\-]+ denotes as many of these combined as we like
     # The sequence will be stored in the parameter <category_name_slug>
-    url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
-        views.show_category, name='show_category'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category'),
     
 ]
