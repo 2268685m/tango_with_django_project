a7ebb74530166b6f07a1d555600918799745c5c1
2268685m <2268685m@student.gla.ac.uk>
2018-02-06 13:37:24 +0000

    End of chapter 8

--- rango/views.py
+++ rango/views.py
@@ -28,13 +28,9 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-	# Construct a dictionary to pass to the template engine as its context.
-	# Note the key boldmessage is the same as {{ boldmessage }} in the template!
 	context_dict = {	'aboutmessage': "This tutorial has been put together by Nicola Mossner.",
 						'catmessage': "I am a cat."	}
-
-	# Return a rendered response to send to the client.
-	# We make use of the shortcut function to make our lives easier. # Note that the first parameter is the template we wish to use.
+                        
 	return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
